a9146bf90686801eb44e75971471c25d6bae7c33
Sahil Goonraz <sahil.goonraz@solirius.com>
2025-09-22 16:43:38 +0100

    Add page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,9 +12,9 @@ module ApplicationHelper
   def full_title(page_title = '')
     base_title = 'QuizApp'
     if page_title.empty?
-      page_title
+      page_title = base_title
     else
-      page_title
+      page_title = base_title + ' - ' + page_title
     end
   end
 
